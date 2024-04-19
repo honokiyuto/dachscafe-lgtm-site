8fc9602d42a3f1dac94230e324dd763d63988c06
honokiyuto <honokiyuto@team-lab.com>
2024-04-19 15:03:07 +0900

    feat: 初稿完成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), icon()]
+});
