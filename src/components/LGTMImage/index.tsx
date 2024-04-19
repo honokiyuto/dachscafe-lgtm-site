160f191a2c95f730cc71d42afb5171bc0494c3be
honokiyuto <honokiyuto@team-lab.com>
2024-04-19 17:52:07 +0900

    feat: 画像の追加、プレースホルダーの追加

---
+++ src/components/LGTMImage/index.tsx
@@ -0,0 +1,54 @@
+import styles from './index.module.scss';
+import { useEffect, useState, type ReactNode } from 'react';
+
+type Props = {
+  src: string;
+};
+
+const Placeholder = () => {
+  return (
+    <div className={styles['placeholder']}>
+      <div className={styles['animated-background']}></div>
+    </div>
+  );
+};
+
+export const LGTMImage = ({ src }: Props) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(`![LGTM](${src})`);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = src;
+    image.onload = () => {
+      // TODO: あとりあえず1秒待つ
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    };
+  }, [src]);
+
+  if (isLoading) {
+    return <Placeholder />;
+  }
+
+  return (
+    <div className={styles['image-container']} onClick={handleClick}>
+      <img
+        className={(isCopied && styles['copied']) || undefined}
+        src={src}
+        loading="lazy"
+        alt="picture"
+      />
+      {isCopied && <p>Copied!!</p>}
+    </div>
+  );
+};
