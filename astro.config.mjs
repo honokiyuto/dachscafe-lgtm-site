27f05455cfebeac2c910bb26cc34e14ab9d3fa55
honokiyuto <honokiyuto@team-lab.com>
2024-04-19 15:11:10 +0900

    feat: actions機能追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()]
+  integrations: [react(), icon()],
+  site: 'https://honokiyuto.github.io',
+  base: '/dachscafe-lgtm-site',
 });
